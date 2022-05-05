fe14cae253b173ac33c02a5ae459abda25a49e25
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-05 16:29:25 -0700

    set state and useEffect for bands

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { getBands } from './services/fetch-utils.js';
 import './App.css';
 
 function App() {
+  const [bands, setBands] = useState([]);
+
+  useEffect(async () => {
+
+    const bandsResponse = await getBands();
+
+    setBands(bandsResponse);
+  });
   return (
     <div className="App">
       
