6a6f0b73fdb68e3476dd060ec7be7db289526ce2
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-06 09:19:17 -0700

    changed spacing and layout in css

--- src/Cars/Car.js
+++ src/Cars/Car.js
@@ -4,11 +4,11 @@ export default function Car({ car }) {
   return (
     <div className='car'>
       <h2>{car.brand} {car.type}</h2>
-      <p>{car.year}</p>
-      <div className='interior'>
+      <p>Year: {car.year}</p>
+      <div className='interior'>Interior:
         {
           car.interior.map((interior, i) =>
-            <p key={i}>{interior}</p>)
+            <p className='interior-item' key={i}>{interior}, </p>)
         }
       </div>
     </div>
