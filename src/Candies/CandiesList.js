894e00252772abedf2a1a18ea2c7f3522ff657ce
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-06 09:31:28 -0700

    completed CandiesList.js, time for Candy.js

--- src/Candies/CandiesList.js
+++ src/Candies/CandiesList.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Candy from './Candy';
 
-export default function CandiesList() {
+export default function CandiesList({ candies }) {
   return (
-    <div>CandiesList</div>
+    <div>
+      {
+        candies.map((candy, i) => 
+          <Candy 
+            key={`${candy.name}-${i}`}
+            candy={candy}
+          />)
+      }
+    </div>
   );
 }
