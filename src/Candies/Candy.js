f1d6ed0015a599e6d3e2888f03ca5922641ef6f2
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-06 09:38:02 -0700

    completed Candy.js all functioning correctly, time for css

--- src/Candies/Candy.js
+++ src/Candies/Candy.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export default function Candy() {
+export default function Candy({ candy }) {
   return (
-    <div>Candy</div>
+    <div>
+      <h2>{candy.name}</h2>
+      <p>Brand: {candy.brand}</p>
+      <p>Year Invented: {candy.year}</p>
+      <div className='category'>
+        Type:
+        {
+          candy.category.map((category, i) =>
+            <p className='category-item' key={i}>{category},</p>)
+        }
+      </div>
+    </div>
   );
 }
